77e09d854174141dc3fc3af276037a34b5813b56
chris94440 <cdemonge91800@gmail.com>
2023-10-14 14:08:12 +0200

    dev daemon

--- resources/tahomalocalapid/tahomalocalapid.py
+++ resources/tahomalocalapid/tahomalocalapid.py
@@ -103,7 +103,7 @@ def loginTahoma():
 		logging.debug("Http code : %s", response.status_code)
 		logging.debug("Response : %s", response.json())
 		logging.debug("Response header : %s", response.headers)
-		logging.debug("Response header[Set-Cookie] : %s", response.headers[Set-Cookie])
+		logging.debug("Response header[Set-Cookie] : %s", response.headers['Set-Cookie'])
 		
 
 		# r = requests.post(url, data = {'key':'value'}, auth=('user', 'passwd'))
