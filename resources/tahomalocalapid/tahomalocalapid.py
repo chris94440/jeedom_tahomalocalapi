3288c81e97fdf8b40a9bae964e15519cd4f323f3
chris94440 <cdemonge91800@gmail.com>
2023-10-15 16:13:42 +0200

    dev daemon

--- resources/tahomalocalapid/tahomalocalapid.py
+++ resources/tahomalocalapid/tahomalocalapid.py
@@ -80,7 +80,7 @@ def listen():
 def httpLog():
 	logging.getLogger("requests").setLevel(logging.ERROR)
 	logging.getLogger("urllib3").setLevel(logging.ERROR)
-	urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+	requests.packages.urllib3.disable_warnings()
 	
 
 # ----------------------------------------------------------------------------
