14b3a2251cb44ef2ee5b791b5e57750dfbbdd71b
chris94440 <cdemonge91800@gmail.com>
2023-12-09 08:32:08 +0100

    add some logs

--- core/class/tahomalocalapi.class.php
+++ core/class/tahomalocalapi.class.php
@@ -726,6 +726,15 @@ private static function createGenericActions($eqLogic, $device) {
 }
 
 private static function removeCmdFromNameOrLogicalId($eqLogic,$cmdName) {
+    log::add(__CLASS__, 'debug','|     ' . __function__ . ' cmdName  ' . $cmdName );
+    foreach($eqLogic->getCmd() as $cmd) {
+        if ($cmd->name() == $cmdName) {
+            log::add(__CLASS__, 'debug','|          -> remove cmd');
+            $cmd->remove();
+            break;
+        }
+    }
+    /*
     $cmd=$eqLogic->getCmd(null, $cmdName);
     if (is_object($cmd)) {
         $cmd->remove();
@@ -737,6 +746,7 @@ private static function removeCmdFromNameOrLogicalId($eqLogic,$cmdName) {
             }
         }
     }
+    */
 }
 
 
